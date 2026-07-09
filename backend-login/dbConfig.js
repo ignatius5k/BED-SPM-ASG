ce2a081eecfe5d1772959b6baec1c01f9cbfdf8f
sheniseee <s10273890@connect.np.edu.sg>
2026-07-09 09:03:32 +0800

    Update dbConfig

--- backend-login/dbConfig.js
+++ backend-login/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  user: bedHawker_user,
-  password: 123,
-  server: localhost,
-  database: hawkerCentreDB,
+  user: "bedHawker_user",
+  password: "123",
+  server: "localhost",
+  database: "hawkerCentreDB",
   trustServerCertificate: true,
   options: {
     port: 1433, // Default SQL Server port
